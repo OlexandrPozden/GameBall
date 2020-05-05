841013789ead1cd9c002520ef5ccf7f0bacfbe3a
OlexandrPozden <pozden.sasha@gmail.com>
2020-05-05 11:57:06 +0300

    Добавьте файлы проекта.

---
+++ Form2.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.ovalPictureBox1 = new OvalPictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ovalPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightSteelBlue;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 450);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(26, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 73);
+            this.textBox1.TabIndex = 3;
+            // 
+            // splitter1
+            // 
+            this.splitter1.BackColor = System.Drawing.Color.Wheat;
+            this.splitter1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(217, 450);
+            this.splitter1.TabIndex = 4;
+            this.splitter1.TabStop = false;
+            // 
+            // ovalPictureBox1
+            // 
+            this.ovalPictureBox1.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ovalPictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ovalPictureBox1.Location = new System.Drawing.Point(322, 43);
+            this.ovalPictureBox1.Name = "ovalPictureBox1";
+            this.ovalPictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.ovalPictureBox1.TabIndex = 2;
+            this.ovalPictureBox1.TabStop = false;
+            this.ovalPictureBox1.Click += new System.EventHandler(this.ovalPictureBox1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.ovalPictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ovalPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private OvalPictureBox ovalPictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Splitter splitter1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        Form3 form = new Form3();
+        private int x;
+        private int y;
+        SolidBrush fon;
+        Random rand;
+        int count = 0;
+        public Form2()
+        {
+            InitializeComponent();
+            x = 50;
+            y = 50;
+        }
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            fon = new SolidBrush(Color.LightSteelBlue);
+            ovalPictureBox1.BackColor = Color.NavajoWhite;
+            rand = new Random();
+            x = rand.Next(225, dataGridView1.Width - 125);
+            y = rand.Next(98, dataGridView1.Height - 125);
+            ovalPictureBox1.Location = new Point(x, y);
+            timer1.Enabled = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int x, y;
+            x = rand.Next(225, dataGridView1.Width - 125);
+            y = rand.Next(98, dataGridView1.Height - 125);
+            ovalPictureBox1.Location = new Point(x, y);
+        }
+
+        private void ovalPictureBox1_Click(object sender, EventArgs e)
+        {
+            count++;
+            textBox1.Text = Convert.ToString(count);
+            Cursor.Position = new Point(350, 175);
+            if (count == 1)
+            {
+                ovalPictureBox1.BackColor = Color.RosyBrown;
+                timer1.Interval = 1000;
+            }
+            else if (count == 2)
+            {
+                ovalPictureBox1.BackColor = Color.Khaki;
+                timer1.Interval = 950;
+            }
+            else if (count == 3)
+            {
+                ovalPictureBox1.BackColor = Color.SteelBlue;
+                timer1.Interval = 900;
+            }
+            else if (count == 4)
+            {
+                ovalPictureBox1.BackColor = Color.Plum;
+                timer1.Interval = 850;
+            }
+            else if (count == 5)
+            {
+                ovalPictureBox1.BackColor = Color.Pink;
+                timer1.Stop();
+                DialogResult dialog = MessageBox.Show("Do you want to play again?",
+                "You win!", MessageBoxButtons.YesNo);
+                if (dialog == DialogResult.Yes)
+                {
+                    textBox1.Text = "0";
+                    count = 0;
+                    timer1.Start();
+
+                }
+                else if (dialog == DialogResult.No)
+                {
+                    this.Hide();
+                    form.ShowDialog();
+                }
+
+            }
+        }
+    }
+}
